4d6c5e383b3b0ff78b5fb7e6cb7c71f6ce4329bc
Lumbha Ram <63182965+erlumbharam@users.noreply.github.com>
2021-03-14 23:37:10 +0530

    Update Online Test, Question 1 Answer.java

--- Online Test, Question 1 Answer.java
+++ Online Test, Question 1 Answer.java
@@ -1,1 +1,23 @@
-[-1,-2,-3]
+myinput='''
+[4,3,2,1] //THIS IS ANSWER
+'''
+
+def maxbad(l):
+  mymax = l[-1]
+  for i in range(-1,-len(l),-1):
+    if l[i] > mymax:
+       mymax = l[i]
+  return(mymax)
+  return(mymax)
+
+import ast
+
+try:
+   myarg =  ast.literal_eval(myinput.strip())
+except:
+   print(False)
+else:
+  try:
+     print(maxbad(myarg) != max(myarg))
+  except:
+     print(False)
